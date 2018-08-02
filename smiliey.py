99d2642ef79f042b9e69a14e2526d2c8a3a326c8
Rishav Agarwal <rishavraj.agarwal@gmail.com>
2018-08-03 00:23:12 +0530

    added python code.

---
+++ smiliey.py
@@ -0,0 +1,83 @@
+from scipy.spatial import distance as dist
+from imutils.video import VideoStream, FPS
+from imutils import face_utils
+import imutils
+import numpy as np
+import time
+import dlib
+import cv2
+
+def smile(mouth):
+    A = dist.euclidean(mouth[3], mouth[9])
+    B = dist.euclidean(mouth[2], mouth[10])
+    C = dist.euclidean(mouth[4], mouth[8])
+    avg = (A+B+C)/3
+    D = dist.euclidean(mouth[0], mouth[6])
+    mar=avg/D
+    return mar
+
+
+COUNTER = 0
+TOTAL = 0
+
+
+shape_predictor= "/Users/rra/Downloads/all/shape_predictor_68_face_landmarks.dat"
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor(shape_predictor)
+
+
+(mStart, mEnd) = face_utils.FACIAL_LANDMARKS_IDXS["mouth"]
+
+print("[INFO] starting video stream thread...")
+vs = VideoStream(src=0).start()
+fileStream = False
+time.sleep(1.0)
+
+fps= FPS().start()
+cv2.namedWindow("test")
+
+while True:
+    frame = vs.read()
+    frame = imutils.resize(frame, width=450)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    rects = detector(gray, 0)
+    for rect in rects:
+        shape = predictor(gray, rect)
+        shape = face_utils.shape_to_np(shape)
+        mouth= shape[mStart:mEnd]
+        mar= smile(mouth)
+        mouthHull = cv2.convexHull(mouth)
+        #print(shape)
+        cv2.drawContours(frame, [mouthHull], -1, (0, 255, 0), 1)
+       
+
+        if mar <= .3 or mar > .38 :
+            COUNTER += 1
+        else:
+            if COUNTER >= 30:
+                TOTAL += 1
+                frame = vs.read()
+                time.sleep(1.0)
+                frame2= frame.copy()
+                img_name = "opencv_frame_{}.png".format(TOTAL)
+                cv2.imwrite(img_name, frame)
+                print("{} written!".format(img_name))
+                cv2.destroyWindow("test")  
+            COUNTER = 0
+
+        cv2.putText(frame, "MAR: {}".format(mar), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
+
+    cv2.imshow("Frame", frame)
+    fps.update()
+    key2 = cv2.waitKey(1) & 0xFF
+    if key2 == ord('q'):
+        break
+
+fps.stop()
+
+print("elasped time: {:.2f}".format(fps.elapsed()))
+print("approx. FPS: {:.2f}".format(fps.fps()))
+
+cv2.destroyAllWindows()
+vs.stop()
+
